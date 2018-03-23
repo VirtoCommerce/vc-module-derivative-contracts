2a8f72d0ce276737a1c329c5cc1c84827b9ba88a
Valeriy Krashenko <vk@virtoway.com>
2018-03-23 15:06:19 +0200

    create virto commerce deriatives module

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("VirtoCommerce")]
+[assembly: AssemblyProduct("Virto Commerce Derivatives Module")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2018")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+
+#else
+[assembly: AssemblyConfiguration("Release")]
+
+#endif
+
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
