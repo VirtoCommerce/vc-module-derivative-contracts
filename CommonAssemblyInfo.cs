25d0601e3ba367e25dabcef3f0cb2d10bcd90e0c
eMazeika <em@virtoway.com>
2018-04-03 17:06:07 +0300

    "0.0.1-alpha" release

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
 
 #if DEBUG
 
