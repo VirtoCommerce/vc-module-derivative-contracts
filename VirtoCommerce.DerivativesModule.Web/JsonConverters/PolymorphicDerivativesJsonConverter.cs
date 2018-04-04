3be81cb8be98241d5568feaf2c400d35c134f86f
eMazeika <em@virtoway.com>
2018-04-04 10:32:52 +0300

    Polymorphic types in API

---
+++ VirtoCommerce.DerivativesModule.Web/JsonConverters/PolymorphicDerivativesJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using VirtoCommerce.DerivativesModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.DerivativesModule.Web.JsonConverters
+{
+    public class PolymorphicDerivativesJsonConverter : JsonConverter
+    {
+        private readonly Type[] _knowTypes = new[] { typeof(Derivative), typeof(DerivativeItem), typeof(DerivativeSearchCriteria) };
+
+        public override bool CanWrite => false;
+        public override bool CanRead => true;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return _knowTypes.Any(x => x.IsAssignableFrom(objectType));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var tryCreateInstance = typeof(AbstractTypeFactory<>).MakeGenericType(objectType).GetMethods().FirstOrDefault(x => x.Name.EqualsInvariant("TryCreateInstance") && x.GetParameters().Length == 0);
+            var result = tryCreateInstance?.Invoke(null, null);
+
+            serializer.Populate(JObject.Load(reader).CreateReader(), result);
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- VirtoCommerce.DerivativesModule.Web/Module.cs
+++ VirtoCommerce.DerivativesModule.Web/Module.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Practices.Unity;
+﻿using System.Web.Http;
+using Microsoft.Practices.Unity;
 using VirtoCommerce.DerivativesModule.Core.Services;
 using VirtoCommerce.DerivativesModule.Data.Repositories;
 using VirtoCommerce.DerivativesModule.Data.Services;
+using VirtoCommerce.DerivativesModule.Web.JsonConverters;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Data.Infrastructure;
@@ -35,6 +37,15 @@ namespace VirtoCommerce.DerivativesModule.Web
             _container.RegisterType<IDerivativeRepository>(new InjectionFactory(c => new DerivativeRepository(_connectionString, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>())));
             _container.RegisterType<IDerivativeService, DerivativeService>();
             _container.RegisterType<IDerivativeSearchService, DerivativeSearchService>();
+        }
+
+        public override void PostInitialize()
+        {
+            base.PostInitialize();
+
+            // enable polymorphic types in API controller methods
+            var httpConfiguration = _container.Resolve<HttpConfiguration>();
+            httpConfiguration.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new PolymorphicDerivativesJsonConverter());
         }
     }
 }
