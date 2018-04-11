092e46ecd7fb76b0162644755847312d6cda7339
Aleksandr Vishniakov <av@virtoway.com>
2018-04-11 12:06:06 +0500

    v1.0.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Derivative Contracts Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2018")]
 
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 
 #if DEBUG
 
