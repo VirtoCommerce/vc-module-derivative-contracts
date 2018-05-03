c142c2df7931ca215d15642781f5a9e0f522db19
konstantin <ksa@virtoway.com>
2018-05-03 14:36:25 +0200

    Added Export/Import #7

---
+++ VirtoCommerce.DerivativeContractsModule.Web/ExportImport/DerivativeContractsExportImport.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using VirtoCommerce.DerivativeContractsModule.Core.Model;
+using VirtoCommerce.DerivativeContractsModule.Core.Services;
+using VirtoCommerce.Platform.Core.ExportImport;
+
+namespace VirtoCommerce.DerivativeContractsModule.Web.ExportImport
+{
+    public class DerivativeContractsExportImport
+    {
+        private const int _batchSize = 20;
+        private readonly JsonSerializer _serializer;
+
+        private readonly IDerivativeContractService _derivativeContractService;
+        private readonly IDerivativeContractSearchService _derivativeContractSearchService;
+
+        public DerivativeContractsExportImport(IDerivativeContractService derivativeContractService, IDerivativeContractSearchService derivativeContractSearchService)
+        {
+            _derivativeContractService = derivativeContractService;
+            _derivativeContractSearchService = derivativeContractSearchService;
+
+            _serializer = new JsonSerializer();
+            _serializer.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            _serializer.Formatting = Formatting.Indented;
+            _serializer.NullValueHandling = NullValueHandling.Ignore;
+        }
+
+        public void DoExport(Stream outStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+        {
+            var progressInfo = new ExportImportProgressInfo { Description = "loading data..." };
+            progressCallback(progressInfo);
+
+            using (StreamWriter sw = new StreamWriter(outStream, Encoding.UTF8))
+            {
+                using (JsonTextWriter writer = new JsonTextWriter(sw))
+                {
+                    writer.WriteStartObject();
+
+                    ExportDerivativeContractEntity(writer, _serializer, manifest, progressInfo, progressCallback);
+                    ExportDerivativeContractItemEntity(writer, _serializer, manifest, progressInfo, progressCallback);
+
+                    writer.WriteEndObject();
+                    writer.Flush();
+                }
+            }
+        }
+
+        private void ExportDerivativeContractEntity(JsonTextWriter writer, JsonSerializer serializer, PlatformExportManifest manifest, ExportImportProgressInfo progressInfo, Action<ExportImportProgressInfo> progressCallback)
+        {
+            progressInfo.Description = "Exporting DerivativeContracts...";
+            progressCallback(progressInfo);
+
+            var totalCount = _derivativeContractSearchService.SearchDerivativeContracts(new DerivativeContractSearchCriteria { Take = 0, Skip = 0 }).TotalCount;
+            writer.WritePropertyName("DerivativeContractsTotalCount");
+            writer.WriteValue(totalCount);
+
+            writer.WritePropertyName("DerivativeContracts");
+            writer.WriteStartArray();
+
+            for (int i = 0; i < totalCount; i += _batchSize)
+            {
+                var results = _derivativeContractSearchService.SearchDerivativeContracts(new DerivativeContractSearchCriteria { Take = _batchSize, Skip = i }).Results;
+
+                foreach (var result in results)
+                {
+                    serializer.Serialize(writer, result);
+                }
+
+                writer.Flush();
+                progressInfo.Description = $"{Math.Min(totalCount, i + _batchSize)} of {totalCount} DerivativeContracts exported";
+                progressCallback(progressInfo);
+            }
+
+            writer.WriteEndArray();
+        }
+
+        private void ExportDerivativeContractItemEntity(JsonTextWriter writer, JsonSerializer serializer, PlatformExportManifest manifest, ExportImportProgressInfo progressInfo, Action<ExportImportProgressInfo> progressCallback)
+        {
+            progressInfo.Description = "Exporting DerivativeContractItems...";
+            progressCallback(progressInfo);
+
+            var totalCount = _derivativeContractSearchService.SearchDerivativeContractItems(new DerivativeContractItemSearchCriteria { Take = 0, Skip = 0 }).TotalCount;
+            writer.WritePropertyName("DerivativeContractItemsTotalCount");
+            writer.WriteValue(totalCount);
+
+            writer.WritePropertyName("DerivativeContractItems");
+            writer.WriteStartArray();
+
+            for (int i = 0; i < totalCount; i += _batchSize)
+            {
+                var results = _derivativeContractSearchService.SearchDerivativeContractItems(new DerivativeContractItemSearchCriteria { Take = _batchSize, Skip = i }).Results;
+
+                foreach (var result in results)
+                {
+                    serializer.Serialize(writer, result);
+                }
+
+                writer.Flush();
+                progressInfo.Description = $"{Math.Min(totalCount, i + _batchSize)} of {totalCount} DerivativeContractItems exported";
+                progressCallback(progressInfo);
+            }
+
+            writer.WriteEndArray();
+        }
+
+        public void DoImport(Stream inputStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+        {
+            var progressInfo = new ExportImportProgressInfo();
+
+            using (StreamReader streamReader = new StreamReader(inputStream))
+            {
+                using (JsonTextReader jsonReader = new JsonTextReader(streamReader))
+                {
+                    while (jsonReader.Read())
+                    {
+                        if (jsonReader.TokenType != JsonToken.PropertyName)
+                            continue;
+
+                        if (jsonReader.Value.ToString() == "DerivativeContracts")
+                        {
+                            jsonReader.Read();
+                            var derivativeContracts = _serializer.Deserialize<DerivativeContract[]>(jsonReader);
+                            progressInfo.Description = $"Importing {derivativeContracts.Length} DerivativeContracts...";
+                            progressCallback(progressInfo);
+                            _derivativeContractService.SaveDerivativeContracts(derivativeContracts);
+                        } else if (jsonReader.Value.ToString() == "DerivativeContractItems")
+                        {
+                            jsonReader.Read();
+                            var derivativeContractItems = _serializer.Deserialize<DerivativeContractItem[]>(jsonReader);
+                            progressInfo.Description = $"Importing {derivativeContractItems.Length} DerivativeContractItem...";
+                            progressCallback(progressInfo);
+                            _derivativeContractService.SaveDerivativeContractItems(derivativeContractItems);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- VirtoCommerce.DerivativeContractsModule.Web/Module.cs
+++ VirtoCommerce.DerivativeContractsModule.Web/Module.cs
@@ -1,17 +1,22 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IO;
+using System.Web.Http;
 using Microsoft.Practices.Unity;
 using VirtoCommerce.DerivativeContractsModule.Core.Services;
 using VirtoCommerce.DerivativeContractsModule.Data.Repositories;
 using VirtoCommerce.DerivativeContractsModule.Data.Services;
+using VirtoCommerce.DerivativeContractsModule.Web.ExportImport;
 using VirtoCommerce.DerivativeContractsModule.Web.JsonConverters;
 using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.ExportImport;
 using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.Platform.Data.Infrastructure;
 using VirtoCommerce.Platform.Data.Infrastructure.Interceptors;
 
 namespace VirtoCommerce.DerivativeContractsModule.Web
 {
-    public class Module : ModuleBase
+    public class Module : ModuleBase, ISupportExportImportModule
     {
         private readonly string _connectionString = ConfigurationHelper.GetConnectionStringValue("VirtoCommerce.DerivativeContracts") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
         private readonly IUnityContainer _container;
@@ -47,6 +52,27 @@ namespace VirtoCommerce.DerivativeContractsModule.Web
             // enable polymorphic types in API controller methods
             var httpConfiguration = _container.Resolve<HttpConfiguration>();
             httpConfiguration.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new PolymorphicDerivativeContractsJsonConverter());
+        }
+
+        public void DoExport(Stream outStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+        {
+            var exportJob = _container.Resolve<DerivativeContractsExportImport>();
+            exportJob.DoExport(outStream, manifest, progressCallback);
+        }
+
+        public void DoImport(Stream inputStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+        {
+            var exportJob = _container.Resolve<DerivativeContractsExportImport>();
+            exportJob.DoImport(inputStream, manifest, progressCallback);
+        }
+
+        public string ExportDescription
+        {
+            get
+            {
+                var settingManager = _container.Resolve<ISettingsManager>();
+                return settingManager.GetValue("DerivativeContracts.ExportImport.Description", string.Empty);
+            }
         }
     }
 }
